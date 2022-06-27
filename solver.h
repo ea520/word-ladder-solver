1558b432cfc2f3ca5e0562749b726662a7a3ce0a
ea520 <57594552+ea520@users.noreply.github.com>
2022-06-27 13:06:24 +0100

    Update solver.h
    removed unnecessary functions

--- solver.h
+++ solver.h
@@ -4,10 +4,7 @@
 #include <list>
 
 std::vector<std::string> load_words(const std::string &filename);
-size_t hamming_distance(const std::string &s1, const std::string &s2);
-std::vector<const std::string *> get_neighbours(const std::string &node);
 void get_distances(const std::string &, const std::string &);
-std::unordered_map<const std::string *, size_t> get_cache();
 std::string to_upper(std::string input);
 
 struct node_t
